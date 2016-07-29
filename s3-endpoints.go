f9495e66fb49b0ee946207ad00b967811ea3172a
Aaron Walker <awwalker3@gmail.com>
2016-07-29 14:07:11 -0700

    Tests: add support for all s3-endpoints (#130)

--- s3-endpoints.go
+++ s3-endpoints.go
@@ -18,14 +18,16 @@ package main
 
 var awsS3EndpointMap = map[string]string{
 	"us-east-1":      "s3.amazonaws.com",
-	"us-west-2":      "s3-us-west-2.amazonaws.com",
 	"us-west-1":      "s3-us-west-1.amazonaws.com",
-	"eu-west-1":      "s3-eu-west-1.amazonaws.com",
-	"eu-central-1":   "s3-eu-central-1.amazonaws.com",
+	"us-west-2":      "s3-us-west-2.amazonaws.com",
+	"ap-south-1":     "s3.ap-south-1.amazonaws.com",
 	"ap-southeast-1": "s3-ap-southeast-1.amazonaws.com",
+	"ap-southeast-2": "s3-ap-southeast-2.amazonaws.com",
 	"ap-northeast-1": "s3-ap-northeast-1.amazonaws.com",
 	"ap-northeast-2": "s3-ap-northeast-2.amazonaws.com",
 	"sa-east-1":      "s3-sa-east-1.amazonaws.com",
+	"eu-central-1":   "s3.eu-central-1.amazonaws.com",
+	"eu-west-1":      "s3-ue-west-1.amazonaws.com",
 	"cn-north-1":     "s3.cn-north-1.amazonaws.com.cn",
 }
 
